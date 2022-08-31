6d00cb4b11aaa210bbc3b02a97430ab74fa7c390
Bob "Wombat" Hogg <rwhogg@google.com>
2022-08-31 15:52:15 +0000

    Add src/ReactUtils.js

---
+++ src/ReactUtils.js
@@ -0,0 +1,15 @@
+const FourOhFour = () => {
+    return(
+        <main>
+            There was an error.
+        </main>
+    )
+}
+
+const Loading = () => {
+    return(
+        <div>Loading!</div>
+    )
+}
+
+export {FourOhFour, Loading}
